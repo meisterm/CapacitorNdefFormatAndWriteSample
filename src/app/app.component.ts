f0d4d095ab29f991d47699a84deb4fc4e43053bf
Holger Martiker <info@einsatzverwaltung.de>
2023-06-02 11:33:51 +0200

    Created working example to show that format and write can't be done in one session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CapacitorNfcService } from './capacitor-nfc.service';
+import { map } from 'rxjs';
+import { NdefMessage, NdefRecord, NfcTag, NfcUtils, UriIdentifierCode } from '@capawesome-team/capacitor-nfc';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,55 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  constructor(private nfc: CapacitorNfcService) {
+
+  }
+
+  public writeNfc() {
+
+
+    this.nfc.scannedTag$
+    .pipe(
+      map(async (x : NfcTag) => {
+
+        console.log('Formatting Tag...');
+        await this.nfc.format();
+        console.log('Formatted!');
+
+        const nfcUtils = new NfcUtils();
+        let records: NdefRecord[] = [];
+        
+        const uriRecord = nfcUtils.createNdefUriRecord({ 
+          uri : 'example.com',
+          identifierCode: UriIdentifierCode.Https
+        });
+
+        records.push(uriRecord.record);
+
+        let msg : NdefMessage = {
+          records : records
+        };      
+
+        console.log('Writing Tag...');
+
+        await this.nfc.write({
+          message: msg
+        });
+
+        console.log('Tag written');
+        console.log('Stopping Scan Session...');
+        await this.nfc.stopScanSession();
+        console.log('Session stopped!');
+
+        return true;
+      })).subscribe(x => {
+
+      });
+
+
+
+      console.log('Starting Scan Session...');
+      this.nfc.startScanSession();
+  }
 }
---
+++ src/app/capacitor-nfc.service.ts
@@ -0,0 +1,105 @@
+import { Injectable, NgZone } from '@angular/core';
+import {
+  Nfc,
+  NfcTag,
+  PollingOption,
+  TransceiveOptions,
+  TransceiveResult,
+  WriteOptions,
+} from '@capawesome-team/capacitor-nfc';
+import { Observable, Subject } from 'rxjs';
+import { Capacitor } from '@capacitor/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CapacitorNfcService {
+
+  private readonly scannedTagSubject = new Subject<NfcTag>();
+  private readonly sessionCanceledSubject = new Subject<void>();
+  private readonly sessionErrorSubject = new Subject<string>();
+
+  constructor(
+    private readonly ngZone: NgZone
+  ) {
+    Nfc.removeAllListeners().then(() => {
+      Nfc.addListener('nfcTagScanned', event => {
+        console.log('nfcTagScanned', { event });
+        this.ngZone.run(() => {
+          this.scannedTagSubject.next(event.nfcTag);
+        });
+      });
+      Nfc.addListener('scanSessionCanceled', () => {
+        console.log('scanSessionCanceled');
+        this.ngZone.run(() => {
+          this.sessionCanceledSubject.next();
+        });
+      });
+      Nfc.addListener('scanSessionError', event => {
+        console.log('scanSessionError', { event });
+        this.ngZone.run(() => {
+          this.sessionErrorSubject.next(event.message);
+        });
+      });
+    });
+  }
+
+  public get scannedTag$(): Observable<NfcTag> {
+    return this.scannedTagSubject.asObservable();
+  }
+
+  public get sessionCanceled$(): Observable<void> {
+    return this.sessionCanceledSubject.asObservable();
+  }
+
+  public get sessionError$(): Observable<string> {
+    return this.sessionErrorSubject.asObservable();
+  }
+
+  public async startScanSession(): Promise<void> {
+    await Nfc.startScanSession({
+      pollingOptions: [PollingOption.iso14443, PollingOption.iso15693],
+    });
+  }
+
+  public async stopScanSession(): Promise<void> {
+    await Nfc.stopScanSession();
+  }
+
+  public async write(options: WriteOptions): Promise<void> {
+    await Nfc.write(options);
+  }
+
+  public async erase(): Promise<void> {
+    await Nfc.erase();
+  }
+
+  public async format(): Promise<void> {
+    await Nfc.format();
+  }
+
+  public transceive(options: TransceiveOptions): Promise<TransceiveResult> {
+    return Nfc.transceive(options);
+  }
+
+  public async openSettings(): Promise<void> {
+    await Nfc.openSettings();
+  }
+
+  public async isSupported(): Promise<boolean> {
+    const { isSupported } = await Nfc.isSupported();
+    return isSupported;
+  }
+
+  public async isEnabled(): Promise<boolean> {
+    if (!this.isAndroid()) {
+      return true;
+    }
+    const { isEnabled } = await Nfc.isEnabled();
+    return isEnabled;
+  }
+
+  private isAndroid(): boolean {
+    return Capacitor.getPlatform() === 'android';
+  }
+}
